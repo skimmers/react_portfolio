5d84ec71b3fd7abbf9250f8552c53fd0f9bc88d1
Sharon Kim <skimilk19@gmail.com>
2021-05-12 15:04:18 -0500

    git init

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+ import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Sharon's react portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
